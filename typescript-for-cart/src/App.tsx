616c6159fdecd90ba7fca420f0ac3af63855d705
yinhsun <yinhsun.chou@ehsn.com.tw>
2022-09-23 10:47:04 +0800

    新增 tailwind config

--- typescript-for-cart/src/App.tsx
+++ typescript-for-cart/src/App.tsx
@@ -1,26 +1,23 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import './App.scss';
+import logo from './assets/logo.svg';
+import { displayProducts, basketProducts } from './Model';
 
+console.log('basketProducts', basketProducts);
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="cart-wrap w-96 mx-auto">
+            <div className="cart-header p-2 text-center">
+                <a href=""><img src={logo} alt=""/></a>
+            </div>
+            {
+                Object.keys(basketProducts).map((basketItem) => {
+                    {console.log('basketItem', basketItem)}
+                    return 1
+                })
+            }
+        </div>
+    )
 }
 
-export default App;
+export default App
